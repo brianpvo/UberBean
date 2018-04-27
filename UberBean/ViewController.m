5aceba7ac249bf3c4a5f52dcdddef9c036115b0f
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-27 14:44:57 -0700

    set up region and span for user location

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -10,7 +10,7 @@
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface ViewController () <MKMapViewDelegate>
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (nonatomic) CLLocationManager *cLLManager;
@@ -25,6 +25,34 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.cLLManager = [[CLLocationManager alloc] init];
     [self.cLLManager requestWhenInUseAuthorization];
+    
+    self.cLLManager.delegate = self;
+    
+    
+    [self.mapView setShowsUserLocation:YES];
+    
+    CLLocationCoordinate2D beginCoord = CLLocationCoordinate2DMake(49.281838, -123.108151);
+    MKCoordinateRegion myRegion = MKCoordinateRegionMakeWithDistance(beginCoord, 500, 500);
+    [self.mapView setRegion:myRegion animated:YES];
+    
+    MKCoordinateSpanMake(0.06, 0.06);
+
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    
+    if (status == (kCLAuthorizationStatusAuthorizedWhenInUse | kCLAuthorizationStatusAuthorizedAlways)) {
+        NSLog(@"made it");
+        [self.cLLManager requestLocation];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    NSLog(@"yo");
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"here");
 }
 
 
