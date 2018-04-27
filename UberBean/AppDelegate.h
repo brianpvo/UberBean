93f8e77601ee36ae77a08ff1cf868609d235e25c
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-27 11:24:36 -0700

    setup project

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Brian Vo on 2018-04-27.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Brian Vo on 2018-04-27.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
